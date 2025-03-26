d16b2f5b538cfb7700d5cc3f5060aa6aa005e080
Sai Kiran Kammari <saikirankammari@Sais-Air.attlocal.net>
2025-03-25 21:41:02 -0700

    changes to array partition

--- ArrayPartition.java
+++ ArrayPartition.java
@@ -3,29 +3,30 @@ class ArrayPartition {
         HashMap<Integer, Integer> freqMap = new HashMap<>();
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-
+        
         for (int num : nums) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
-
+        
         int sum = 0;
         boolean flag = true;
-
+        
         for (int i = min; i <= max; i++) {
             int count = freqMap.getOrDefault(i, 0);
-            int timesToAdd = count / 2;
-
-            if (flag) {
-                sum += timesToAdd * i;
+            while (count > 0) {
+                if (flag) {
+                    sum += i;
+                }
+                count--;
+                flag = !flag;
             }
-
-            flag = !flag;
         }
 
         return sum;
     }
+
 }
 
 //Time Complexity: O(n)
